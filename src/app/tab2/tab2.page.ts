fea8685aa1994e8f0adbf96ac0586bd1575bde75
Arianerodokas <barbosaarianerodokas@gmail.com>
2023-03-16 12:27:30 -0300

    terminar funções

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -7,6 +7,35 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
+valendo = 1;
+time1 = 0;
+time2 = 0;
+ftime1 = 0;
+ftime2 = 0;
   constructor() {}
 
+  mudarNumero(numero: number){
+
+      this.valendo = numero;
+
+  }
+
+  addbotao1(){
+
+    this.time1 += this.valendo
+    this.valendo = 1
+    if(this.time1 >= 12) {
+    this.ftime1 += 1
+    this.time1 = 0
+    this.time2 = 0
+
+    }
+  }
+
+  subbotao1() {
+
+    this.time1 -= this.valendo
+    this.valendo
+
+  }
 }
